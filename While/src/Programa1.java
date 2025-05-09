cf8acbc9985f362ce8ff89e3018b536918e47b62
Julio Sanchez <julio.sanchez@ptvgroup.com>
2025-05-09 08:21:05 +0200

    Ejemplo de bucle while

---
+++ While/src/Programa1.java
@@ -0,0 +1,9 @@
+public class Programa1 {
+    public static void main(String[] args) {
+        boolean puedeSeguir = false;
+        while(puedeSeguir){
+            System.out.println("Hola");
+        }
+        System.out.println("Adios");
+    }
+}
---
+++ While/src/Programa2.java
@@ -0,0 +1,10 @@
+public class Programa2 {
+    public static void main(String[] args) {
+        int contador = 5;
+        while(contador <= 20){
+            System.out.println(contador);
+            contador = contador + 2;
+        }
+        System.out.println("Adios");
+    }
+}
