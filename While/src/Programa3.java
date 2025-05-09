de02db584aa886f24e26097412ec878a1fcbfc80
Julio Sanchez <julio.sanchez@ptvgroup.com>
2025-05-09 09:56:07 +0200

    Ejemplo de bucle while actualziado

--- While/src/Programa3.java
+++ While/src/Programa3.java
@@ -2,15 +2,12 @@ public class Programa3 {
     public static void main(String[] args) {
         int contador = 1;
         int suma = 0;
-        int producto = 1;
         while(contador <= 3){
             System.out.println(contador);
             suma = suma + contador;
-            producto = producto * contador;
             contador = contador + 1;
         }
         System.out.println("La suma es " + suma);
-        System.out.println("El producto es " + producto);
         System.out.println("Adios");
 
     }
