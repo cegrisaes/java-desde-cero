f21be9d40e20a975b16ce1bb338dafd441446ba3
Juleo Sanchez <losingbeforewinning@gmail.com>
2025-08-23 17:57:51 +0200

    Agregar constructores para Perro

--- OOP_3/src/Perro.java
+++ OOP_3/src/Perro.java
@@ -2,6 +2,20 @@ public class Perro {
     public String nombre;
     public String raza;
 
+    public Perro(String nombre, String raza){
+        this.nombre = nombre;
+        this.raza = raza;
+    }
+
+    public Perro(String nombre){
+        this.nombre = nombre;
+        raza = "Cruzado";
+    }
+
+    public Perro(){
+        nombre = "Sin nombre";
+        raza = "Cruzado";
+    }
     public void ladrar(){
         System.out.println(nombre + ", de raza " + raza + " está ladrando: GUAUUU GUAUUU");
     }
