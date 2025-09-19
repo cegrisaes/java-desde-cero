9f4ebcdab36cbf43cae1483c75b39a02f1a2e93d
Juleo Sanchez <losingbeforewinning@gmail.com>
2025-09-19 12:04:51 +0200

    Tratando con el valor NULL

---
+++ OOP_7/src/Persona.java
@@ -0,0 +1,58 @@
+public class Persona {
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad){
+       setNombre(nombre);
+       setEdad(edad);
+    }
+
+    public Persona(String nombre){
+        setNombre(nombre);
+        setEdad(18);
+    }
+
+    public Persona(int edad){
+        setEdad(edad);
+        setNombre("Desconocido");
+    }
+
+    public Persona(){
+        setNombre("Desconocido");
+        setEdad(18);
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        if(nombre == null){
+            this.nombre = "Desconocido";
+            return;
+        }
+        if(nombre.trim().isEmpty()){
+            System.out.println("El parámetro nombre no es válido");
+            this.nombre = "Desconocido";
+        }else{
+            this.nombre = nombre;
+        }
+    }
+
+    public int getEdad(){
+        return edad;
+    }
+
+    public void setEdad(int edad){
+        if(edad < 0 || edad > 120){
+            System.out.println("El parámetro edad no es válido");
+            this.edad = 18;
+        }else{
+            this.edad = edad;
+        }
+    }
+
+    public void saludar(){
+        System.out.println("¡Hola! Me llamo " + nombre + " y tengo " + edad + " años. Gusto en conocerte." );
+    }
+}
